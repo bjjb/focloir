d0f1c778faddbe37cf7f65a4c05ef7a53b21ce87
Bryan <jj@Buckley.(none)>
2009-12-17 22:29:45 +0000

    Added search form

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,6 +2,10 @@ class WordsController < ApplicationController
   def show
     @word = Word.find_or_create_by_word(params[:id])
   end
+
+  def create
+    redirect_to Word.find_or_create_by_word(params[:word])
+  end
   
   def destroy
     @word = Word.find_by_word(params[:id])
